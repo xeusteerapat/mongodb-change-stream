2f0e71869a5c3733132d2067c9aee5dabdb5a852
xeusteerapat <xeus085@gmail.com>
2021-02-27 20:05:35 +0700

    [FIX] :lipstick: add chakra-ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import socketIOClient from 'socket.io-client';
+import {
+  Box,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Container,
+  Center,
+} from '@chakra-ui/react';
+import dayjs from 'dayjs';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [scores, setScores] = useState([]);
+
+  useEffect(() => {
+    const socket = socketIOClient('http://localhost:5500/api/socket');
+
+    socket.on('newScore', latestScores => {
+      setScores(latestScores);
+    });
+
+    const fetchScores = async () => {
+      try {
+        const { data } = await axios.get(
+          'http://localhost:5500/api/leaderboard'
+        );
+
+        setScores(data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    fetchScores();
+  }, []);
+
+  const fetchLatestScores = () => {
+    if (scores.length === 0) {
+      return (
+        <>
+          <p>Loading...</p>
+        </>
+      );
+    }
+
+    return (
+      <Box>
+        <h1>Leaderboard</h1>
+        <Container>
+          <Center>
+            <Table size='md'>
+              <Thead>
+                <Tr>
+                  <Th>ID</Th>
+                  <Th isNumeric>Score</Th>
+                  <Th isNumeric>Date</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {scores.map(score => (
+                  <Tr key={score._id}>
+                    <Td>{score.userId}</Td>
+                    <Td>{score.score}</Td>
+                    <Td>{dayjs(score.createdAt).format('YYYY:MM:DD')}</Td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </Center>
+        </Container>
+      </Box>
+    );
+  };
+
+  return fetchLatestScores();
 }
 
 export default App;
